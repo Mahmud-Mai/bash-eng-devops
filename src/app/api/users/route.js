9973f9884dab1de0003f13a18c16cc74c9a20935
Mahmud Mai <mahmudx00@gmail.com>
2023-10-07 19:37:53 +0100

    Create a controller for Users

---
+++ src/app/api/users/route.js
@@ -0,0 +1,86 @@
+import { NextResponse } from "next/server";
+import connectdb from "../../../utils/db";
+import User from "@/models/user.js";
+
+export const GET = async (req, res) => {
+  try {
+    await connectdb();
+    const users = await User.find().lean();
+    if (!users) {
+      return new NextResponse("No users found", { status: 404 });
+    }
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database Error!", { status: 500 });
+  }
+};
+
+export const POST = async (req, res) => {
+  try {
+    await connectdb();
+    const userData = await req.json();
+
+    // Validate the user data.
+    const user = new User(userData);
+    await user.validate();
+
+    // Create a new user and save it to the database.
+    await user.save();
+
+    // Return the saved user.
+    return res.json(user);
+  } catch (error) {
+    return new NextResponse(error.message, { status: 400 });
+  }
+};
+
+export const PUT = async (req, res) => {
+  try {
+    await connectdb();
+    const userData = await req.json();
+    const userId = req.query.id;
+
+    // Find the user and update it with the new data.
+    const user = await User.findByIdAndUpdate(userId, userData);
+    if (!user) {
+      return new NextResponse("No user found", { status: 404 });
+    }
+
+    // Return the updated user.
+    return res.json(user);
+  } catch (error) {
+    return new NextResponse(error.message, { status: 400 });
+  }
+};
+
+export const DELETE = async (req, res) => {
+  try {
+    await connectdb();
+    const userId = req.query.id;
+
+    // Find the user and delete it from the database.
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return new NextResponse("No user found", { status: 404 });
+    }
+
+    // Return a success message.
+    return res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    return new NextResponse(error.message, { status: 500 });
+  }
+};
+
+export const GET_BY_ID = async (req, res) => {
+  try {
+    await connectdb();
+    const userId = req.query.id;
+    const user = await User.findById(userId).lean();
+    if (!user) {
+      return new NextResponse("No user found", { status: 404 });
+    }
+    return new NextResponse(JSON.stringify(user), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database Error!", { status: 500 });
+  }
+};
