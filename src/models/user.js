eb1179ecfbba685cf6d3bef9a4491baccf86d577
Mahmud Mai <mahmudx00@gmail.com>
2023-10-08 21:04:10 +0100

    temp changes to ensure working protoyp

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,26 @@
 import { Schema, model, models } from "mongoose";
 
 const userSchema = new Schema({
-  name: {
-    type: String,
-    required: "Please provide a name",
-    trim: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    required: "Please provide a valid email address",
-  },
   phoneNumber: Number,
-  password: {
-    type: String,
-    required: "Please provide a valid password",
-    minlength: 4,
-  },
 });
 
 const User = models.User || new model("User", userSchema);
 export default User;
+
+// name: {
+//   type: String,
+//   required: "Please provide a name",
+//   trim: true,
+// },
+// email: {
+//   type: String,
+//   unique: true,
+//   lowercase: true,
+//   trim: true,
+//   required: "Please provide a valid email address",
+// },
+// password: {
+//   type: String,
+//   required: "Please provide a valid password",
+//   minlength: 4,
+// },
