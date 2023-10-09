040a94c75076b7e8f596fccf913b47a74c86c470
Mahmud Mai <mahmudx00@gmail.com>
2023-10-09 08:55:24 +0100

    Create endpoint for products/id

---
+++ src/app/api/products/[id]/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import connectdb from "../../../../utils/db.js";
+import Product from "@/models/product.js";
+
+export const GET = async (req, { params }) => {
+  const productId = params.id;
+  try {
+    await connectdb();
+    const product = await Product.findById(productId)
+      .populate(["category", "unit"])
+      .lean();
+    console.log(`🚀 ~ GET ~ product:`, product);
+    if (!product) {
+      return new NextResponse("No product found", { status: 404 });
+    }
+    return new NextResponse(JSON.stringify(product), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Internal Server Error!", { status: 500 });
+  }
+};
