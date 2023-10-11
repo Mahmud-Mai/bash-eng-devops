f87f3f5412968eff93ba38c8f541ff062c1ab90d
Mahmud Mai <mahmudx00@gmail.com>
2023-10-11 12:22:57 +0100

    Add Analytics to App layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Farm Direct",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className="relative w-[80%] mx-auto">
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
